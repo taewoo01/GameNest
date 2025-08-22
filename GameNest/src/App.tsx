31bae26798e516846793b75bb86e853069ea0acd
00leeseungjun <sck07013@naver.com>
2025-08-22 19:29:17 +0900

    fix(#64) : 글쓰기 페이지 코드 수정

--- GameNest/src/App.tsx
+++ GameNest/src/App.tsx
@@ -4,7 +4,7 @@ import Nav from './layout/Nav'
 import GameList from './pages/GameList'
 import Community from './pages/Community'
 import News from './pages/News'
-// import WriteEditor from './pages/WriteEditor'
+import WriteEditor from './pages/WriteEditor'
 import PostDetail from './pages/PostDetail'
 import SearchResults from './pages/SearchResults'
 import CategoryPage from './pages/CategoryPage'
@@ -20,20 +20,20 @@ function App() {
     <>
     <Router>
       <Nav />
-      <Routes>
-        <Route path="/" element={<GameList/>}/>
-        <Route path="/community" element={<Community/>}/>
-        <Route path="/news" element={<News/>}/>
-        {/* <Route path="/write" element={<WriteEditor/>}/> */}
-        <Route path="/community/post/:id" element={<PostDetail/>}/>
-        <Route path="/search" element={<SearchResults/>}/>
-        <Route path="/category/:categoryType/:categoryValue" element={<CategoryPage/>}/>
-        <Route path="/game/:id" element={<GameDetail/>}/>
-        <Route path="/myposts" element={<MyPosts/>}/>
-        <Route path="/scraps" element={<MyScraps/>}/>
-        <Route path="/liked" element={<LikedGames/>}/>
-        <Route path="/my-comments" element={<MyComments/>}/>
-      </Routes>
+        <Routes>
+            <Route path="/" element={<GameList/>}/>
+            <Route path="/community" element={<Community/>}/>
+            <Route path="/news" element={<News/>}/>
+            <Route path="/write" element={<WriteEditor/>}/>
+            <Route path="/community/post/:id" element={<PostDetail/>}/>
+            <Route path="/search" element={<SearchResults/>}/>
+            <Route path="/category/:categoryType/:categoryValue" element={<CategoryPage/>}/>
+            <Route path="/game/:id" element={<GameDetail/>}/>
+            <Route path="/myposts" element={<MyPosts/>}/>
+            <Route path="/scraps" element={<MyScraps/>}/>
+            <Route path="/liked" element={<LikedGames/>}/>
+            <Route path="/my-comments" element={<MyComments/>}/>
+        </Routes>
     </Router>
     </>
   )
