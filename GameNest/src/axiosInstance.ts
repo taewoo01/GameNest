6bfcf3d4dd5164219e0b0b6c0bb4d73ead89dad6
taewoo01 <133633356+taewoo01@users.noreply.github.com>
2025-09-02 19:43:41 +0900

    Update axiosInstance.ts

--- GameNest/src/axiosInstance.ts
+++ GameNest/src/axiosInstance.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.MODE === "development"
-    ? "/api"
-    : "/api"; 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL,
